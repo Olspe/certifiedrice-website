3771e54ae009c277a07d9b549469a19b89c626be
BossFreind1234 <vincentredtube@gmail.com>
2022-05-22 10:43:48 -0400

    rice emoji tilt

--- pages/index.jsx
+++ pages/index.jsx
@@ -27,6 +27,8 @@ export default function Home() {
             <div className="header" id="header">
                 <h1>Certified Rice</h1>
 
+                <h1 className="ricebowl">🍚</h1>
+
                 <img src="https://avatars.githubusercontent.com/certifiedrice" alt="profile picture" className="profile-picture"/>
 
                 <p>Indie game developer and Front-end Web Developer</p>
