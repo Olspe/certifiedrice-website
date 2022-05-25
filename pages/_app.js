a0f9afb02d7a6a4d25fa5cf2aa7623e73f9e80a7
BossFreind1234 <vincentredtube@gmail.com>
2022-05-25 16:40:57 -0400

    framer js animations

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
+import {motion, AnimatePresence} from "framer-motion";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
-      <Component {...pageProps} />
+      <AnimatePresence>
+      <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
+          pageInitial: {
+              opacity: 0
+          },
+          pageAnimate: {
+              opacity: 1
+          },
+          pageExit: {
+              backgroundColor: 'white',
+              filter: `invert()`,
+              opacity: 0
+
+          }
+      }}>
+          <Component {...pageProps} />
+      </motion.div>
+      </AnimatePresence>
   )
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import {motion} from "framer-motion";
 
 export default function Home() {
     return (
